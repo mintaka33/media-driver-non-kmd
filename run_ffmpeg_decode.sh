e3413760fe9fca42a78b985b169873a612169b0e
mintaka33 <mintaka33@outlook.com>
2018-07-02 22:47:26 -0400

    add scripts to run ffmpeg decode/transcode tests

---
+++ run_ffmpeg_decode.sh
@@ -0,0 +1,3 @@
+export LD_PRELOAD=./build_driver/media_driver/linux/ult/libdrm_mock/libdrm_mock.so
+
+./build_ffmpeg/ffmpeg_g -hwaccel vaapi -hwaccel_output_format vaapi -i ~/test.mp4 -vframes 2000 -f null -
