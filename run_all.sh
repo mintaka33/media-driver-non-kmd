a954a075edf3b867d35d628fd6d01b23a032826e
mintaka33 <mintaka33@outlook.com>
2018-07-02 23:04:48 -0400

    update run_all script

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,8 @@
+./run_libva_utils_decode.sh
+./run_libva_utils_encode.sh
 
 ./run_ffmpeg_decode.sh
 ./run_ffmpeg_transcode.sh
-
-#./run_libva_utils_decode.sh
-./run_libva_utils_encode.sh
 
 ./run_sample_decode.sh
 ./run_sample_multi_transcode.sh
