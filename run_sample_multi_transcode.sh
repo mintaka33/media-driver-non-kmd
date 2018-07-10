073cb7930634451c7f8f2611b570fb6c1baab7b7
mintaka33 <mintaka33@outlook.com>
2018-07-09 22:41:12 -0400

    update runing scripts

--- run_sample_multi_transcode.sh
+++ run_sample_multi_transcode.sh
@@ -1,4 +1,4 @@
 
 export LD_PRELOAD=./build_driver/media_driver/linux/ult/libdrm_mock/libdrm_mock.so
 
-./build_msdk/__bin/Debug/sample_multi_transcode -i::h264 ~/test.264 -o::h264 ~/out.264
+./build_msdk/__bin/Debug/sample_multi_transcode -i::h264 ~/test3.264 -o::h264 ~/out.264
